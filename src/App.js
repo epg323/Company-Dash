f133cc3b9bdc1a0510fe8a1192ccd657c0ceb021
e p <ep@es-MacBook-Pro.local>
2020-03-06 17:14:24 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Thermometor from 'react-thermometer-ecotropy';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <h1>Emerging Markets BigCommerce Dash</h1>
+      </div>
+      <div className="Therm">
+        <Thermometor
+          theme="light"
+          value="1891"
+          max="2274"
+          size="large"
+          height="300"
+        />
+      </div>
     </div>
   );
 }
